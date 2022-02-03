c9bf656c8f7873ae84e175636aba18cf599526ed
d-smith <doug.smith.mail@gmail.com>
2022-02-03 06:44:24 -0800

    queries and seed updates

--- positions/src/main/java/com/example/myapp/App.java
+++ positions/src/main/java/com/example/myapp/App.java
@@ -11,7 +11,15 @@ import java.util.Map;
 
 public class App
 {
-    public static void main( String[] args ) {
+    static String[] symbols = {
+            "DWAC",
+            "SNDL",
+            "CEI",
+            "FAMI",
+            "KMDN"
+    };
+
+    public static void main( String[] args ) throws Exception {
         System.out.println( "Hello World!" );
 
         Region region = Region.US_WEST_2;
@@ -21,18 +29,32 @@ public class App
 
         dynamoDBClient.listTables().tableNames().forEach(t -> System.out.println(t));
 
-        PutItemResponse pir = dynamoDBClient.putItem(
-                PutItemRequest.builder()
-                        .item(
-                                Map.of("symbol", AttributeValue.builder().s("AAPL").build(),
-                                "account-id", AttributeValue.builder().s("x0001").build(),
-                                        "quantity", AttributeValue.builder().n("10").build()
-                                )
-                        )
-                        .tableName("positions")
-                        .build()
-        );
+        //Add 1000000 accounts with positions
+        for(int i=0; i < 1000000;i++) {
 
-        System.out.println(pir.toString());
+            String account = String.format("x%d",i);
+            int holdings = (int) (Math.random() * 5) + 1;
+            for(int j=0; j< holdings;j++) {
+
+
+                String shares = String.format("%d", (int) (Math.random() * 500) + 1);
+
+                PutItemResponse pir = dynamoDBClient.putItem(
+                        PutItemRequest.builder()
+                                .item(
+                                        Map.of("symbol", AttributeValue.builder().s(symbols[j]).build(),
+                                                "account-id", AttributeValue.builder().s(account).build(),
+                                                "quantity", AttributeValue.builder().n(shares).build()
+                                        )
+                                )
+                                .tableName("positions")
+                                .build()
+                );
+
+                Thread.sleep(50);
+            }
+            System.out.println(String.format("create %d holdings for %s account", holdings, account));
+        }
+
     }
 }
---
+++ positions/src/main/java/com/example/myapp/Queries.java
@@ -0,0 +1,47 @@
+package com.example.myapp;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import java.util.Map;
+
+
+public class Queries {
+
+    static String[] symbols = {
+            "DWAC",
+            "SNDL",
+            "CEI",
+            "FAMI",
+            "KMDN"
+    };
+
+    public static void main(String... args) {
+
+        Region region = Region.US_WEST_2;
+        DynamoDbClient dynamoDBClient = DynamoDbClient.builder()
+                .region(region)
+                .build();
+
+        for(int i = 0; i < symbols.length; i++) {
+
+            QueryRequest queryRequest = QueryRequest.builder()
+                    .limit(1000000)
+                    .indexName("position-by-symbol")
+                    .tableName("positions")
+                    .keyConditions(Map.of("symbol", Condition.builder().comparisonOperator(
+                            ComparisonOperator.EQ
+                    )
+                            .attributeValueList(AttributeValue.builder().s(symbols[i]).build())
+                            .build()))
+                    .select(Select.SPECIFIC_ATTRIBUTES).attributesToGet("account-id","quantity")
+                    .build();
+            QueryResponse qr = dynamoDBClient.query(queryRequest);
+            System.out.println(String.format("counted %d positions for %s", qr.count(), symbols[i]));
+            qr.items().forEach(item -> {
+                System.out.println(String.format("account %s has %s shares",item.get("account-id").s(),item.get("quantity").n()));
+            });
+        }
+    }
+}
